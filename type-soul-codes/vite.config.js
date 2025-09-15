11ccc620a0a79c88a54119952e6e3c2523ea3851
你的GitHub用户名 <GitHub>
2025-09-15 12:04:01 +0800

    修改robots-1

--- type-soul-codes/vite.config.js
+++ type-soul-codes/vite.config.js
@@ -20,7 +20,8 @@ export default defineConfig({
   build: {
     rollupOptions: {
       external: []
-    }
+    },
+    copyPublicDir: true
   },
   assetsInclude: ['**/*.webp'],
   publicDir: 'public'
